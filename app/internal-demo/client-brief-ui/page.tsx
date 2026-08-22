e0251108c611fe21c488cfc547b0adfa55b2a629
mastosidharta777-cmyk <mastosidharta777@gmail.com>
2026-08-22 23:53:11 +0700

    Add client brief form preview page

---
+++ app/internal-demo/client-brief-ui/page.tsx
@@ -0,0 +1,7 @@
+import { ClientBriefForm } from "@/components/client-brief-form";
+
+export const dynamic = "force-dynamic";
+
+export default function ClientBriefUiPreviewPage() {
+  return <ClientBriefForm />;
+}
